f6733fd2c9c6a89d859b09487adc9e6b4b5913c1
Viraj Gaur <vgaur01@qub.ac.uk>
2021-11-30 13:49:37 +0000

    Finished the starting page

--- digital_transformation/lib/Start.dart
+++ digital_transformation/lib/Start.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:show_up_animation/show_up_animation.dart';
 
 import 'Login.dart';
 
@@ -7,21 +9,55 @@ class Start extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
 
-      body: Column(
+      body:Column(
 
         children: [
+          ShowUpAnimation(
+            delayStart: Duration(milliseconds: 500),
+            animationDuration: Duration(milliseconds: 800),
+            curve: Curves.decelerate,
+            direction: Direction.vertical,
+            offset: 0.5,
+            child:Container(
+              padding: EdgeInsets.fromLTRB(20.0,20.0,40.0,20.0),
+              child: Image.asset('Assets/QueenÔÇÖs Red Logo - Landscape.png',height: 200 ,width: 200,),
+            ),
+          ),
+
           Row(
 
             children: [
+
               Container(
-                padding: EdgeInsets.fromLTRB(20.0,300.0,40.0,20.0),
-                child: Text('Welcome to our ',
-                  style: TextStyle(
-                    fontFamily: 'Alegreya',
-                    fontSize: 40.0,
-                    color: Colors.deepPurple,
-                    fontWeight: FontWeight.bold,
+                padding: EdgeInsets.fromLTRB(20.0,70.0,40.0,20.0),
+                child: ShowUpAnimation(
+                  delayStart: Duration(milliseconds: 1200),
+                  animationDuration: Duration(milliseconds: 800),
+                  curve: Curves.easeIn,
+                  direction: Direction.horizontal,
+                  offset: 0.5,
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      TypewriterAnimatedText(
+                        'Welcome to our',
+                        textStyle: const TextStyle(
+                          color: Color.fromRGBO(255, 0, 0, 1.0),
+                          fontFamily: 'Raleway',
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        cursor: '',
+
+                        speed: const Duration(milliseconds: 100),
+                      ),
+                    ],
+
+                    totalRepeatCount: 1,
+                    pause: const Duration(milliseconds: 1000),
+                    displayFullTextOnTap: true,
+                    stopPauseOnTap: true,
                   ),
                 ),
               ),
@@ -32,36 +68,57 @@ class Start extends StatelessWidget {
           Row(
             children: [
               Container(
-                padding: EdgeInsets.fromLTRB(20.0,0.0,40.0,20.0),
-                child: Text('prototype',
-                  style: TextStyle(
-                    fontFamily: 'Alegreya',
-                    fontSize: 40.0,
-                    color: Colors.deepPurple,
-                    fontWeight: FontWeight.bold,
+                padding: EdgeInsets.fromLTRB(20.0,0.0,40.0,100.0),
+                child:ShowUpAnimation(
+                  delayStart: Duration(milliseconds: 1800),
+                  animationDuration: Duration(milliseconds: 800),
+                  curve: Curves.easeInBack,
+                  direction: Direction.horizontal,
+                  offset: 0.5,
+                  child: AnimatedTextKit(
+                    animatedTexts: [
+                      TypewriterAnimatedText(
+                        'Prototype',
+                        textStyle: const TextStyle(
+                          color: Color.fromRGBO(255, 0, 0, 1.0),
+                          fontFamily: 'Raleway',
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        cursor: '',
+
+                        speed: const Duration(milliseconds: 100),
+                      ),
+                    ],
+
+                    totalRepeatCount: 1,
+                    pause: const Duration(milliseconds: 1000),
+                    displayFullTextOnTap: true,
+                    stopPauseOnTap: true,
                   ),
                 ),
-              ),
+                ),
             ],
           ),
           Row(
             mainAxisAlignment : MainAxisAlignment.spaceEvenly,
             children: [
-              ElevatedButton(onPressed: (){},
-                  child: Text('Sign up')
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Login()),
-                  );
-                },
+              ShowUpAnimation(
+                delayStart: Duration(milliseconds: 2500),
+                animationDuration: Duration(milliseconds: 800),
+                curve: Curves.bounceOut,
+                direction: Direction.vertical,
+                offset: 0.5,
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Login()),
+                    );
+                  },
+                  child: Text('Lets get started',),
 
-
-                child: Text('Sign in') ,
-
-
+                ),
               ),
             ],
           ),
@@ -72,3 +129,5 @@ class Start extends StatelessWidget {
 
   }
 }
+
+
