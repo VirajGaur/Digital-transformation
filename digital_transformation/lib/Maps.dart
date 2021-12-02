6d466638ac1257fa1efd9514b6e4d43cbd274e1d
Ronan Broderick <rbroderick01@qub.ac.uk>
2021-12-02 18:11:45 +0000

    Added store maps

--- digital_transformation/lib/Maps.dart
+++ digital_transformation/lib/Maps.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:show_up_animation/show_up_animation.dart';
 
 import 'Grocery.dart';
 
@@ -10,8 +11,9 @@ class Maps extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Column(
-            children: [Container(
-              padding: EdgeInsets.fromLTRB(20.0,100.0,40.0,500.0),
+            children: [
+              Container(
+              padding: EdgeInsets.fromLTRB(20.0,100.0,40.0,60.0),
               child: Text('Maps',
                 style: TextStyle(
                   fontFamily: 'Alegreya',
@@ -21,6 +23,17 @@ class Maps extends StatelessWidget {
                 ),
               ),
             ),
+              ShowUpAnimation(
+                delayStart: Duration(milliseconds: 200),
+                animationDuration: Duration(milliseconds: 400),
+                curve: Curves.decelerate,
+                direction: Direction.vertical,
+                offset: 0.5,
+                child:Container(
+                  padding: EdgeInsets.fromLTRB(20.0,20.0,20.0,0.0),
+                  child: Image.asset('Assets/TescoMap.JPG',height: 300 ,width: 300,),
+                ),
+              ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
@@ -34,7 +47,6 @@ class Maps extends StatelessWidget {
 
                       child: Text('Grocery')
                   ),
-
                 ],
               ) ,
             ],
